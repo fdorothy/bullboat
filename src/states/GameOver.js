c7ad43f7fa9dd355d1d265cb51fb7e090ed57c89
Fredric Dorothy <fredricdorothy@gmail.com>
2017-06-01 22:08:18 -0500

    Added a gameover screen

---
+++ src/states/GameOver.js
@@ -0,0 +1,31 @@
+/* globals __DEV__ */
+import Phaser from 'phaser'
+
+export default class extends Phaser.State {
+  init () {}
+  preload () {}
+
+  create () {
+    const bannerText = 'GAME OVER'
+    let banner = this.add.text(this.world.centerX, this.game.height / 2.0, bannerText)
+    banner.font = 'Bangers'
+    banner.padding.set(10, 16)
+    banner.fontSize = 40
+    banner.fill = '#77BFA3'
+    banner.smoothed = false
+    banner.anchor.setTo(0.5)
+
+    var retry = game.make.sprite(this.world.centerX, this.game.height / 2.0, 'retry');
+    retry.inputEnabled = true;
+    retry.input.priorityID = 1;
+    retry.input.useHandCursor = true;
+    retry.events.onInputDown.add(onretry, this);
+  }
+
+  onretry () {
+    this.state.start('Game');
+  }
+
+render () {
+  }
+}
